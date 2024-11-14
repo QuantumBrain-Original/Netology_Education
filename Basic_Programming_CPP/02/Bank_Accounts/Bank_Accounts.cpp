497c7699ddc0c26ba6b80b16bd787ae74543ce46
QuantumBrain-Original <Quantum.Brain.Original@gmail.com>
2024-11-14 05:57:12 +0300

    Add files via upload

---
+++ Basic_Programming_CPP/02/Bank_Accounts/Bank_Accounts.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include "Windows.h"
+
+struct Bank_Account
+{
+    int account_ID;
+    std::string person_name;
+    double all_money;
+};
+
+void Bank_Account_Set_Money(Bank_Account &person, int current_money) {
+    person.all_money = current_money;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    double userMoney;
+    Bank_Account account;
+
+    std::cout << "Введите номер счёта: " << std::endl;
+    std::cin >> account.account_ID;
+    std::cout << "Введите имя владельца: " << std::endl;
+    std::cin >> account.person_name;
+    std::cout << "Введите баланс: " << std::endl;
+    std::cin >> account.all_money;
+
+    system("cls");
+
+    std::cout << "Введите новый баланс: " << std::endl;
+    std::cin >> userMoney;
+
+    Bank_Account_Set_Money(account, userMoney);
+
+    std::cout << "Ваш счёт: " << account.account_ID << " " << account.person_name << " " << account.all_money << std::endl;
+}
